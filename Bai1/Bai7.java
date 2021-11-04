a8e118dec56549b28ec2dc3c20c55bf005df7bbf
Dat Tran <tran.x.dat1996@gmail.com>
2021-11-04 14:33:27 +0700

    Loi chao

---
+++ Bai1/Bai7.java
@@ -0,0 +1,12 @@
+package Bai1;
+
+import java.util.Scanner;
+
+public class Bai7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Your name is?");
+        String name = scanner.nextLine();
+        System.out.println("Hello " + name);
+    }
+}
