021b71049aa75ab0192db54bb213b3d7d3118462
Dat Tran <tran.x.dat1996@gmail.com>
2021-11-04 14:01:55 +0700

    Khai bao va su dung bien

---
+++ Bai1/Bai2.java
@@ -0,0 +1,18 @@
+package Bai1;
+
+public class Bai2 {
+    public static void main(String[] args) {
+        int i = 10;
+        float j = 20.5f;
+        double k = 20.5;
+        boolean b = true;
+        char c = 'a';
+        String s = "Hà Nội";
+        System.out.println("i= " + i);
+        System.out.println("j= " + j);
+        System.out.println("k= " + k);
+        System.out.println("b= " + b);
+        System.out.println("c= " + c);
+        System.out.println("s= " + s);
+    }
+}
