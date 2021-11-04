11f8cfce45641e587e59786a263606286921fe1e
Dat Tran <tran.x.dat1996@gmail.com>
2021-11-04 14:22:01 +0700

    Kiem tra nam nhuan

---
+++ Bai1/Bai5.java
@@ -0,0 +1,25 @@
+package Bai1;
+
+import java.util.Scanner;
+
+public class Bai5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int year;
+        System.out.println("Enter a year: ");
+        year = scanner.nextInt();
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    System.out.printf("%d is a leap year", year);
+                } else {
+                    System.out.printf("%d is NOT a leap year", year);
+                }
+            } else {
+                System.out.printf("%d is a leap year", year);
+            }
+        } else {
+            System.out.printf("%d is NOT a leap year", year);
+        }
+    }
+}
