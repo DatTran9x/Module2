2c2741d852b48ab87545bbc7b3991480be09a356
Dat Tran <tran.x.dat1996@gmail.com>
2021-11-04 14:29:26 +0700

    Kiem tra can nang

---
+++ Bai1/Bai6.java
@@ -0,0 +1,25 @@
+package Bai1;
+
+import java.util.Scanner;
+
+public class Bai6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double weight;
+        double height;
+        System.out.println("Weight is:");
+        weight = scanner.nextDouble();
+        System.out.println("Height is:");
+        height = scanner.nextDouble();
+        double bmi = weight / (height * height);
+        if (bmi < 18.5) {
+            System.out.println("Underweight");
+        } else if (bmi < 25) {
+            System.out.println("Normal");
+        } else if (bmi < 30) {
+            System.out.println("Overweight");
+        } else if (30 <= bmi) {
+            System.out.println("Obese");
+        }
+    }
+}
