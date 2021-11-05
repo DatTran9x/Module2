9214f5543c0407f18c3a651cd8e78baaad08dbbc
Dat Tran <tran.x.dat1996@gmail.com>
2021-11-05 15:46:01 +0700

    Tìm giá trị nhỏ nhất trong mảng

---
+++ Bai3/Bai10.java
@@ -0,0 +1,36 @@
+package Bai3;
+
+import java.util.Scanner;
+
+public class Bai10 {
+    public static void main(String[] args) {
+        int size;
+        int[] array;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.print("Enter a size:");
+            size = scanner.nextInt();
+            if (size > 20)
+                System.out.println("Size does not exceed 20");
+        } while (size > 20);
+        array = new int[size];
+        int i = 0;
+        while (i < array.length) {
+            System.out.print("Enter element " + (i + 1) + ": ");
+            array[i] = scanner.nextInt();
+            i++;
+        }
+        System.out.printf("%-20s%s", "Elements in array: ", "");
+        for (int k : array) {
+            System.out.print(k + "\t");
+        }
+        System.out.println();
+        int min = array[0];
+        for (int j = 1; j < array.length; j++) {
+            if (array[j] < min) {
+                min = array[j];
+            }
+        }
+        System.out.println("Min is: " + min);
+    }
+}
