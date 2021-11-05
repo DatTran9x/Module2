832551c001a08856ba8399cb0827c3a059411d8b
Dat Tran <tran.x.dat1996@gmail.com>
2021-11-05 14:48:44 +0700

    Fixed

--- Bai1/Bai6.java
+++ Bai1/Bai6.java
@@ -5,12 +5,10 @@ import java.util.Scanner;
 public class Bai6 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double weight;
-        double height;
         System.out.println("Weight is:");
-        weight = scanner.nextDouble();
+        double weight = scanner.nextDouble();
         System.out.println("Height is:");
-        height = scanner.nextDouble();
+        double height = scanner.nextDouble();
         double bmi = weight / (height * height);
         if (bmi < 18.5) {
             System.out.println("Underweight");
