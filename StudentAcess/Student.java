f48072610fbd44a2d1897fd01baa700f9a6e5390
Dat Tran <tran.x.dat1996@gmail.com>
2021-11-09 10:33:25 +0700

    Xây dựng lớp chỉ ghi trong Java

---
+++ StudentAcess/Student.java
@@ -0,0 +1,17 @@
+package StudentAcess;
+
+public class Student {
+    String name;
+    String classes;
+
+    public Student() {
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    void setClasses(String classes) {
+        this.classes = classes;
+    }
+}
---
+++ StudentAcess/Test.java
@@ -0,0 +1,12 @@
+package StudentAcess;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        student1.setName("Nam");
+        student1.setClasses("C1300I5");
+        System.out.println(student1.name);
+        System.out.println(student1.classes);
+    }
+}
