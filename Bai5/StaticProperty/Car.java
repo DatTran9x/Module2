9a627e31af7caca0779c24813e3b55d4273908fd
Dat Tran <tran.x.dat1996@gmail.com>
2021-11-09 10:24:09 +0700

    Static Property

--- Bai5/StaticProperty/Car.java
+++ Bai5/StaticProperty/Car.java
@@ -4,13 +4,9 @@ public class Car {
     public static int numberOfCars;
     private final String name;
     private final String engine;
-
     public Car(String name, String engine) {
-
         this.name = name;
-
         this.engine = engine;
-
         numberOfCars++;
     }
 }
