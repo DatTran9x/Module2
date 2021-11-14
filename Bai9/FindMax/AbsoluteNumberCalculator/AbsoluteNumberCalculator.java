2bb482e928f9c39720f62bd9740d077f7cae5fb8
Admin <tran.x.dat1996@gmail.com>
2021-11-14 16:32:29 +0700

    [Thực hành] Tìm giá trị tuyệt đối

---
+++ Bai9/FindMax/AbsoluteNumberCalculator/AbsoluteNumberCalculator.java
@@ -0,0 +1,9 @@
+package Bai9.FindMax.AbsoluteNumberCalculator;
+
+public class AbsoluteNumberCalculator {
+    public static int findAbsolute(int number){
+        if(number < 0)
+            return  -number;
+        return number;
+    }
+}
