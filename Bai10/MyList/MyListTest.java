0c78e560a31f60d1686c78da81810b9cad4ddf5c
Admin <tran.x.dat1996@gmail.com>
2021-11-16 09:10:47 +0700

    [Bài tập] Triển khai các phương thức của ArrayList theo thư viện java.util.ArrayList

---
+++ Bai10/MyList/MyListTest.java
@@ -0,0 +1,13 @@
+package Bai10.MyList;
+
+public class MyListTest {
+    public static void main(String[] args) {
+        MyList<Integer> list = new MyList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.remove(2);
+        System.out.println(list.size());
+
+    }
+}
