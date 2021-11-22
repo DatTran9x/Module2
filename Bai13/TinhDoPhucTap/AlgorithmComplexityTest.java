1f025fce900f1644983718364203cacf787326b1
Admin <tran.x.dat1996@gmail.com>
2021-11-22 22:30:38 +0700

    [Optional] [Thực hành] Tính độ phức tạp của thuật toán

---
+++ Bai13/TinhDoPhucTap/AlgorithmComplexityTest.java
@@ -0,0 +1,34 @@
+package Bai13.TinhDoPhucTap;
+
+import java.util.Scanner;
+
+public class AlgorithmComplexityTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String inputString = scanner.nextLine();
+        /*codes below here */
+        int[] frequentChar = new int[255];                         //{1}
+        for (int i = 0; i < inputString.length(); i++) {           //{2}
+            /* Tìm ví trí tương ứng của ký tự trong bảng ASCII */
+            int ascii = inputString.charAt(i);               //{3}
+            /* Tăng giá trị tần suất */
+            frequentChar[ascii] += 1;                              //{4}
+        }
+        int max = 0;                                             //{5}
+        char character = (char) 255; /* empty character */      //{6}
+        for (int j = 0; j < 255; j++) {                         //{7}
+            if (frequentChar[j] > max) {                        //{8}
+                max = frequentChar[j];                          //{9}
+                character = (char) j;                           //{10}
+            }
+        }
+        System.out.println("The most appearing letter is '" + character + "' with a frequency of " + max + " times");
+    }
+
+    //O(1)=O(1);
+    //O(3,4)=O(n);
+    //0(8,9,10)=0(255);
+    //Neu n>255 thi do phuc tap se la 0(n);
+    //Neu khong thi do phuc tap se la 0(255);
+}
