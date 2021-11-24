9a829d325c68ec46dc32a01e0d84de7c6e104688
Admin <tran.x.dat1996@gmail.com>
2021-11-24 08:54:41 +0700

    [Bài tập] Cài đặt thuật toán sắp xếp chèn

---
+++ Bai14/insertSort/InsertSort.java
@@ -0,0 +1,24 @@
+package Bai14.insertSort;
+
+public class InsertSort {
+    public static void insertionSort(int[] array) {
+        int pos, x;
+        for (int i = 1; i < array.length; i++) { //đoạn array[0] đã sắp xếp
+            x = array[i];
+            pos = i;
+            while (pos > 0 && x < array[pos - 1]) {
+                array[pos] = array[pos - 1]; // đổi chỗ
+                pos--;
+            }
+            array[pos] = x;
+        }
+    }
+
+    static int[] list = {2, 3, 2, 5, 6, 1, -2, 3, 14, 12};
+
+    public static void main(String[] args) {
+        insertionSort(list);
+        for (int i = 0; i < list.length; i++)
+            System.out.print(list[i] + " ");
+    }
+}
