aea74824398c14e4f233addd39d2bcf9a2151135
Admin <tran.x.dat1996@gmail.com>
2021-11-29 14:07:45 +0700

    [Thực hành] Racing Cars

--- Bai16/CopyFile/copyFile/CopyFileExample.java
+++ Bai16/CopyFile/copyFile/CopyFileExample.java
@@ -11,15 +11,11 @@ import java.io.OutputStream;
         public static void main(String[] args) throws IOException {
             InputStream inStream = null;
             OutputStream outStream = null;
-
             try {
                 inStream = new FileInputStream(new File("C:\\Module2.1\\src\\Bai16\\CopyFile\\scource.txt"));
-                outStream = new FileOutputStream(new
-                        File("C:\\Module2.1\\src\\Bai16\\CopyFile\\target.txt"));
-
+                outStream = new FileOutputStream(new File("C:\\Module2.1\\src\\Bai16\\CopyFile\\target.txt"));
                 int length;
                 byte[] buffer = new byte[1024];
-
                 // copy the file content in bytes
                 while ((length = inStream.read(buffer)) > 0) {
                     outStream.write(buffer, 0, length);
